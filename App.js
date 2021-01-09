cd443a77d6a9d97667ef0d4a645aea6d2a110439
androidtrivedi1 <android1@trivediinfotech.com>
2021-01-09 16:46:04 +0530

    test

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
       <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
+          name="Home222"
           component={HomeScreen}
         />
         <Stack.Screen
